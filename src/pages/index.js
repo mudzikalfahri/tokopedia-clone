e562ccb6c40c97c3bef0068eca39b17e2dba7a7f
unknown <alfahrimudzik@gmail.com>
2021-07-18 08:53:55 +0700

    added loading screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import Category from "../components/Category";
 import Footer from "../components/Footer";
@@ -7,19 +8,38 @@ import Product from "../components/Product";
 
 export default function Home({products}) {
 
+  const [loading,setLoading] = useState(false);
+  useEffect(() =>{
+    setLoading(true)
+    const timing = setTimeout(() => {
+    setLoading(false)
+    }, 3000);
+    return () => clearTimeout(timing)
+  }, []);
+
   return (
     <div>
       <Head>
         <title>Tokopedia</title>
       </Head>
-
-      <Header />
-      <main className="max-w-screen-2xl mx-auto mt-32">
-        <Banner />
-        <Category />
-        <Product products={products}/>
-      </main>
-      <Footer />
+      { loading ? (
+        <div className="w-full h-screen flex justify-center place-items-center relative">
+        <div className="w-11 h-11 rounded-full animate-ping absolute translate-x-1/2 translate-y-1/2 bg-gradient-to-t from-tokped_green to-yellow-200"></div>
+        <div className="w-11 h-11 rounded-full animate-bounce bg-gradient-to-b from-tokped_green to-yellow-200"></div>
+      </div>
+      ) : (
+        <>
+          <Header />
+          <main className="max-w-screen-2xl mx-auto mt-32">
+            <Banner />
+            <Category />
+            <Product products={products}/>
+          </main>
+          <Footer />
+        </>
+      )}
+      
+      
       
     </div>
   );
